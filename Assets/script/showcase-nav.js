d4d9f8148114c6806955a5562e191cc21c7001a1
Hudson Hancock <hudsonhancock@gmail.com>
2021-08-15 22:13:21 -0400

    Added showcase page and linked js

---
+++ Assets/script/showcase-nav.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  console.log("ready!");
+
+  $("#nav-btn").click(() => {
+    $(".menu").removeClass("invisible");
+    $(".showcase").addClass("blur");
+    $("footer").addClass("blur");
+
+    $("#nav-btn").addClass("invisible");
+  });
+
+  $("#down-arrow").click(() => {
+    $(".menu").removeClass("invisible");
+    $(".showcase").removeClass("blur");
+    $("footer").removeClass("blur");
+    $(".menu").addClass("invisible");
+
+    $("#nav-btn").removeClass("invisible");
+  });
+});
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 1.1,
+  centeredSlides: true,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
